bae55584c38ff492cd1e0ff41a8307fe7090543b
Phạm Hải Sơn <21dh112887@st.huflit.edu.vn>
2024-03-23 16:04:06 +0700

    Add project files.

---
+++ ClothingShop/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothingShop.Models;
+using PagedList;
+
+namespace ClothingShop.Controllers
+{
+    
+    public class CategoryController : Controller
+    {
+        ClothingStore1Entities db = new ClothingStore1Entities();
+        // GET: Category
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var prodList = db.Products.ToList();
+            return View(prodList.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult ListProductCate(int id, int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            ViewBag.Category = db.Categories.FirstOrDefault(c => c.CategoryID == id).CategoryName;
+            var prodList = db.Products.Where(p => p.CategoryID == id).ToList();
+            return View(prodList.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult ListProductNSX(int id, int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            ViewBag.NSX = db.NSXes.FirstOrDefault(c => c.IDnsx == id).TenNSX;
+            var prodList = db.Products.Where(p => p.IDnsx == id).ToList();
+            return View(prodList.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Search(string inputSearch, int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var result = db.Products.Where(s => s.ProductName.Contains(inputSearch)).ToList();
+            return View(result.ToPagedList(pageNum, pageSize));
+        }
+    }
+}
