2598bffb45a37531786fcf02fefb6353ed07e9e0
Phạm Hải Sơn <21dh112887@st.huflit.edu.vn>
2024-03-29 01:53:09 +0700

    Update Facede

--- ClothingShop/Models/Facade Pattern/ProductFacade.cs
+++ ClothingShop/Models/Facade Pattern/ProductFacade.cs
@@ -5,17 +5,33 @@ using System.Web;
 
 namespace ClothingShop.Models.Facade_Pattern
 {
+    /// <summary>
+    /// Lớp ProductFacade là một phần của mẫu thiết kế Facade.
+    /// Nó cung cấp một giao diện đơn giản cho việc tạo và quản lý thông tin sản phẩm.
+    /// </summary>
     public class ProductFacade
     {
+        // Khai báo biến thành viên
         ProductImage productImage;
         ProductInfo productInfo;
+
+        // Constructor của lớp ProductFacade
         public ProductFacade(string FileImage, int productID, int? categoryID, int? nsxID, string productName, string descriptionPro, decimal? PRICE, string imgPro, int? quantity)
         {
+            // Khởi tạo đối tượng ProductImage với tham số truyền vào
             productImage = new ProductImage(FileImage);
-            productInfo = new ProductInfo(productID, categoryID, nsxID, productName, descriptionPro, PRICE, imgPro, quantity); 
+
+            // Khởi tạo đối tượng ProductInfo với tham số truyền vào
+            productInfo = new ProductInfo(productID, categoryID, nsxID, productName, descriptionPro, PRICE, imgPro, quantity);
         }
+
+        /// <summary>
+        /// Phương thức ConstructProduct nhận một đối tượng Product và sử dụng các đối tượng ProductImage và ProductInfo để xây dựng thông tin cho sản phẩm.
+        /// </summary>
+        /// <param name="product">Đối tượng Product cần được xây dựng thông tin</param>
         public void ConstructProduct(Product product)
         {
+            // Gọi các phương thức SetImage và SetInfo của các đối tượng productImage và productInfo để đặt thông tin cho sản phẩm
             productImage.SetImage(product);
             productInfo.SetInfo(product);
         }
