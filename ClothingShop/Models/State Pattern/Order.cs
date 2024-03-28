063875a0674dfda3cb77c82c3dc4aa8224b01756
Phạm Hải Sơn <21dh112887@st.huflit.edu.vn>
2024-03-29 02:01:58 +0700

    Update State

---
+++ ClothingShop/Models/State Pattern/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothingShop.Models.State_Pattern
+{
+    public class Order
+    {
+        private IOrderState _orderState;
+        public Order(IOrderState state)
+        {
+            _orderState = state;
+        }
+        public void SetState(IOrderState state)
+        {
+            _orderState = state;
+        }
+        public void ApproveOrder(DonHang order)
+        {
+            _orderState.ApproveOrder(order);
+        }
+    }
+}
---
+++ ClothingShop/Models/State Pattern/PendingState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothingShop.Models.State_Pattern
+{
+    public class PendingState : IOrderState
+    {
+        public void ApproveOrder(DonHang order)
+        {
+            order.Status = 1;
+        }
+    }
+}
